56b620f9d969b0931c37c56cbfa3c649ffcd19f4
Kang Wang <wk1984@users.noreply.github.com>
2025-12-26 20:43:18 +0800

    Update install-deps.sh

--- torch-wrapper/env/install-deps.sh
+++ torch-wrapper/env/install-deps.sh
@@ -3,6 +3,8 @@
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 
+echo $SCRIPT_DIR
+
 
 #wget "https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.11.0%2Bcpu.zip"
 #unzip "libtorch-cxx11-abi-shared-with-deps-1.11.0+cpu.zip"
